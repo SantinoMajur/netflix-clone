63ec89d0ddae331bfdc2fdf554292793e2263fed
SantinoMajur <dem65109@gmail.com>
2022-10-02 22:37:38 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// sudo npm install -g firebase-tools
+const firebaseConfig = {
+    apiKey: "AIzaSyDFQCpjw-sN_WkqNlXey7j9mFkUZkacsGI",
+    authDomain: "netflix-clone-1b1ab.firebaseapp.com",
+    projectId: "netflix-clone-1b1ab",
+    storageBucket: "netflix-clone-1b1ab.appspot.com",
+    messagingSenderId: "193049006622",
+    appId: "1:193049006622:web:d65d5bc9aa02878e68d40f",
+    measurementId: "G-PSX5QFC82B"
+  };
